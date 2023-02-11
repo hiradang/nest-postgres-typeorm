eb89785bca7843bdf237c86aa8aed9b199de283a
hiradang <sunnybinhdang@gmail.com>
2023-02-11 10:34:08 +0700

    [content] middlewares and custom exceptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +12,7 @@ import { QuizModule } from './modules/quiz/quiz.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ApiTokenCheckMiddleWare } from './common/middleware/api-token-check.middleware';
 
 @Module({
   imports: [
@@ -19,4 +25,11 @@ import { AuthModule } from './modules/auth/auth.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiTokenCheckMiddleWare).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
---
+++ src/common/middleware/api-token-check.middleware.ts
@@ -0,0 +1,20 @@
+import {
+  BadRequestException,
+  HttpException,
+  NestMiddleware,
+  HttpStatus,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { ApiTokenPaymentException } from '../exception/api-token-payment.exception';
+
+export class ApiTokenCheckMiddleWare implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(req.headers);
+    if (req.headers['api-token'] !== 'my-token') {
+      //   throw new BadRequestException('The token does not match');
+      //   throw new HttpException('My response', HttpStatus.PAYMENT_REQUIRED);
+      throw new ApiTokenPaymentException();
+    }
+    next(); // After do checking, use next() to continue sending request to controller
+  }
+}
